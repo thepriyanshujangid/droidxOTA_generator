5a468fb97b1ebe7c002738eae49662f7437db9fa
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2024-02-26 13:54:58 +0530

    ota-json: Drop Unneeded targets
    also adapt json structure for DroidX-UI OTA

--- generate_json.sh
+++ generate_json.sh
@@ -3,29 +3,20 @@
 
 #modify values below
 #leave blank if not used
-maintainer="Name (nickname)" 
-oem="OEM" 
-device="device codename" #ex: guacamole
-devicename="name of device" #ex: OnePlus 7 Pro
-zip="ricedroid zip" #ex: crDroidAndroid-<android version>-<date>-<device codename>-v<droidx version>.zip
-buildtype="type" #choose from Testing/Alpha/Beta/Weekly/Monthly
-forum="" #https link (mandatory)
-gapps="" #https link (leave empty if unused)
-firmware="" #https link (leave empty if unused)
-modem="" #https link (leave empty if unused)
-bootloader="" #https link (leave empty if unused)
-recovery="" #https link (leave empty if unused)
-paypal="" #https link (leave empty if unused)
-telegram="" #https link (leave empty if unused)
-dt="" #https://github.com/DroidX-UI-Devices/device_<oem>_<device_codename>
-commondt="" #https://github.com/DroidX-UI-Devices/device_<orm>_<SOC>-common
-kernel="" #https://github.com/DroidX-UI-Devices/kernel_<oem>_<SOC>
+maintainer="Name (nickname)"
+oem="xiaomi"
+device="lancelot" 
+devicename="Redmi 9" 
+zip="droidx-2.0-20240225-1116-OFFICIAL-lancelot-Gapps.zip"
+forum=""
+gapps=""
+recovery=""
+telegram=""
 
-
-#don't modify from here
+# Don't modify from here
 script_path="`dirname \"$0\"`"
-zip_name=$script_path/out/target/product/$device/$zip
-buildprop=$script_path/out/target/product/$device/system/build.prop
+zip_name=out/target/product/$device/$zip
+buildprop=out/target/product/$device/system/build.prop
 
 if [ -f $script_path/$device.json ]; then
   rm $script_path/$device.json
@@ -34,14 +25,8 @@ fi
 linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
 timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
 zip_only=`basename "$zip_name"`
-md5=`md5sum "$zip_name" | cut -d' ' -f1`
 sha256=`sha256sum "$zip_name" | cut -d' ' -f1`
 size=`stat -c "%s" "$zip_name"`
-version=`echo "$zip_only" | cut -d'-' -f5`
-v_max=`echo "$version" | cut -d'.' -f1 | cut -d'v' -f2`
-v_min=`echo "$version" | cut -d'.' -f2`
-version=`echo $v_max.$v_min`
-
 echo '{
   "response": [
     {
@@ -49,24 +34,13 @@ echo '{
         "oem": "'$oem'",
         "device": "'$devicename'",
         "filename": "'$zip_only'",
-        "download": "https://sourceforge.net/projects/ricedroid/files/'$device'/'$v_max'.x/'$zip_only'/download",
+        "download": "https://sourceforge.net/projects/droidxui-releases/files/'$device'/'$v_max'.x/'$zip_only'/download",
         "timestamp": '$timestamp',
-        "md5": "'$md5'",
         "sha256": "'$sha256'",
         "size": '$size',
-        "version": "'$version'",
-        "buildtype": "'$buildtype'",
         "forum": "'$forum'",
         "gapps": "'$gapps'",
-        "firmware": "'$firmware'",
-        "modem": "'$modem'",
-        "bootloader": "'$bootloader'",
-        "recovery": "'$recovery'",
-        "paypal": "'$paypal'",
-        "telegram": "'$telegram'",
-        "dt": "'$dt'",
-        "common-dt": "'$commondt'",
-        "kernel": "'$kernel'"
+        "telegram": "'$telegram'"
     }
   ]
 }' >> $device.json
